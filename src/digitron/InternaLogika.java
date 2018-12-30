ead0c7cbd84730906a62f5f31ada3eccee0f1243
Dragan <neki@tamo.mejl>
2018-12-30 13:38:58 +0100

    Sa zadacima :)

--- src/digitron/InternaLogika.java
+++ src/digitron/InternaLogika.java
@@ -2,55 +2,117 @@ package digitron;
 
 import java.util.Stack;
 
+
+/*
+ * ZADACI
+ * 
+ * Dodati ostale operacije 
+ *        --Obratite paznju kod oduzimanja i deljenja. Nacin na koji radimo obrne redosled operanada. Ako smo uneli 23 + 2, 2 je zadnji element na stacku pa kada uradimo pop() + pop() mi, u stvari racunamo 2 + 23
+ *          Nije problem kod sabiranja i mnozenja, ali za druge dve operacije to mora drugacije. Moj predolg je da u jednu promenljivu uradite prvi pop() pa onda pop() - promenljiva da bi redosled bio u redu
+ * Dodati memoriju
+ *        --Trebace nam jos jedno stanje u klasi, koje ce drzati vrednost. Kada korisnik pritisne M+ dodajemo sta god da je na displeju (sto ce da bude i zadnji element u steku) na to stanje. Mozemo koristit peek() da ne sklonimo vrednost sa
+ *        stacka. M- ce, naravno da oduzme. MR cita vrednost (jednostavan predlog pop() da sklonimo sta je bilo zadnje na stacku pa push() da stavimo novu vrednost). MC je jednostavno stavi na 0
+ * +/- 
+ *      --Ovo dugme bi trebalo da obrne znak zadnjoj vrednosti. Predlog, peek() da vidimo je li pozitivna, ako je onda 0 - pop() i to samo push() na stack :) ako je neg onda mozemo Math.abs 
+ * %
+ *     --Procentni racun bi trebao da radi na slican nacin kao =. Kada se pritisne, nebitno je koju je operaciju pre korisnik uneo dobija se procentni racun. Npr 50 + 25 % bi iznosilo 12.5 kao i 50 * 25 %
+ *       Predlog je da, kao i jednako, proveri da li ima dva elementa u stacku i onda samo izracuna procenat, bez obzira na zadnnju operaciju
+ * .   
+ *     --Ovo bude zabavno :) Treba da podelimo posao na dva dela. Prvo, da napravimo da program radi sa brojevima koji nisu celi, pa tek onda da vidimo kako da ih i unosimo kao takve. Prvi deo je laksi. Ako stavimo da su nas stack i
+ *       nase stanje za ulaz double program ce da radi u redu. No, ostalo nam je da mozemo da i unesemo broj koji nije ceo.
+ *       Ideja: kada se pomeramo pre zareza racunamo prosliBroj * 10 + noviBroj. Pa ako imamo 5 a kucamo 2 dobijamo 5 * 10 + 2 tj 52. Ako posle kucamo 1 dobijamo 52 * 10 + 1 = 521. Kada hocemo da se krecemo iza tacke, recimo, 
+ *       imamo 5.0 i kucamo 2. Da bi dobili sto zelimo racunamo 5 + 2 * 0.1 i dobijamo 5.2. Kucamo 3, treba da racunamo 5.2 + 3 * 0.01 da bi dobili 5.23. Formula nam je, znaci, prosliBroj + noviBroj * 10 na minus tacno koliko je odmaknut od tacke
+ *       Za prvi broj iza tacke 10 na minus prvu, zatim na minus drugu i tako dalje. Nacin na koji bih ja ovo resio je da bi imao stanje koje mi kaze da li sam iza zareza, recimo neki int koji je na 0 kada smo sa leve strane zareza. Kada
+ *       trebamo da predjemo na desnu, umanjimo ga i pre no sto racunamo nas novi broj na liniji 49 proverimo je li to stanje manje od nule. Ako jeste onda koristimo ovu drugu forumulu i taman mozemo da korisitimo to stanje jer ce ono biti 
+ *       -1 za prvu cifru, pa dignemo 10 na njega (Math.pow(10, nekoStanje)) i umanjimo ga opet. Kada korisnik pritisne neku operaciju ili = i tome slicno samo ga stavimo ponovo na 0 :)  
+ */
+
+
+
 public class InternaLogika 
 {
-	static Stack<Long> unos = new Stack<Long>(); 
-	
-	public static void procesirajUnos(char unos)
+	static Stack<Long> unos = new Stack<Long>(); //Stack u koji cemo da unosimo brojeve
+	static char zadnjaOperacija = '0'; //Treba nam da znamo sta je bila zadnja pritisnuta operacija ako korisnik pritisne =
+	static int broj; //Stanje za ulaz 
+
+	public static String procesirajUnos(char unos)
 	{
 		try
 		{
-			long broj = Integer.parseInt(String.valueOf(unos));
-			if (InternaLogika.unos.isEmpty())
+			broj = Integer.parseInt(String.valueOf(unos)); //Pokusavamo da parsiramo simbol koji smo dobili kao broj
+
+			if (InternaLogika.unos.isEmpty()) //Ako smo uspeli, proverimo da li imamo vec nesto u steku
 			{
-				InternaLogika.unos.push(broj);
-			} else if (InternaLogika.unos.peek() == 0)
-			{
-				InternaLogika.unos.pop();
-				InternaLogika.unos.push(broj);
+				InternaLogika.unos.push((long)broj); //Ako nemamo, samo unosimo direktno broj koji smo dobili
 			} else
 			{
-				if (String.valueOf(InternaLogika.unos.peek()).length() <= 15)
-				{
-					InternaLogika.unos.push(InternaLogika.unos.pop() * 10 + broj);
-				}
+				InternaLogika.unos.push(InternaLogika.unos.pop()*10 + broj); //Ako nije prazan, onda vadimo zadnji broj sa stacka, pomeramo ga na desetine i dodajemo novi. Pa, ako je
+				//bio broj 5 na stacku i korisnik kuca 2 dobijamo 5 * 10 + 2 = 52 :) 
 			}
-			
-		} catch (NumberFormatException e) 
+			return String.valueOf(InternaLogika.unos.peek()); //Vracamo zadnju vrednost sa stacka da bi se prikazala na ekranu naseg digitrona
+		} catch (NumberFormatException e)
 		{
-			if (unos == '+')
+			String izlaz = "0"; //Ako nismo uspeli da parsiramo broj u liniji 42 onda dolazimo ovde. Pripremamo nesto da posaljemo nazad za ekran, za svaki slucaj stavimo odmah 0 ako se bas nista ne dogodi
+			//Npr, ako korisnik odmah pri pokretanju programa pritisne + apsolutno nista ne radimo od ovog koda dole.
+			if (!InternaLogika.unos.isEmpty())
 			{
-				if (InternaLogika.unos.size() == 1)
+				switch (unos) //Proveravamo sta je unos
 				{
-					InternaLogika.unos.push((long)0);
-				} else
-				{
-					InternaLogika.unos.push(InternaLogika.unos.pop() + 
-							                InternaLogika.unos.pop());
+				case '+': 
+					zadnjaOperacija = '+'; //Postavljamo informaciju o operaciji koju radimo
+					if (InternaLogika.unos.size() < 2) //Imamo li dva broja za sabiranje na stacku?
+					{
+						izlaz = String.valueOf(InternaLogika.unos.peek()); //Ako nemamo, samo ostavljamo taj jedan broj koji imamo za prikaz
+					} else
+					{
+						InternaLogika.unos.push(InternaLogika.unos.pop() + InternaLogika.unos.pop()); //U slucaju da imamo dva broja radimo tri stvari u jednoj liniji
+						//Npr ako na stacku imamo 3 i 2, prvo ce se izvrsiti popovi tako da cemo imati
+						//InternaLogika.unos.push(3 + 2); i posto pop() uklanja vrednost sa stacka, stack ce da bude prazan
+						//Zatim ce push samo da gurne zbir nazad na stack
+						izlaz = String.valueOf(InternaLogika.unos.peek());  //Za ekran samo jos pogledamo sta nam je upravo dobijeni zbir
+					}
+					InternaLogika.unos.push((long)0); //Zavrsili sabiranje ili ne, ako smo pritisnuli + moramo da predejmo na novi broj
+					//Nas program uvek radi sa zadnjim brojem na stacku. Kada hocemo da unesemo, na primer 23 + 5. Mi kada kucamo 2 to postaje prvi broj na stacku. Kada kucamo 3 po formuli
+					//na liniji 49 dobijamo 2*10 + 3 tj. 23. Zatim, pritisnemo + i u tom momentu guramo 0 na stack. Pa na stacku imamo 23 i 0. Zatim unesemo 5, pa imamo 0 * 10 + 5 tj 5 :)
+					break;
+				case '=':
+					if (zadnjaOperacija != '0') //Proveravamo je li uposte neka operacija pritisnuta
+					{
+						switch (zadnjaOperacija)  //Ako jeste, gledamo koja je
+						{
+						case '+':
+							if (InternaLogika.unos.size() == 2) //Proveravamo imamo li dovoljno brojeva za racunanje
+							{
+
+								InternaLogika.unos.push(InternaLogika.unos.pop() + InternaLogika.unos.pop()); //Racunamo
+							} 
+							izlaz = String.valueOf(InternaLogika.unos.peek());
+							InternaLogika.unos.push((long)0); 
+							break;
+						}
+						break;
+					}
 				}
-			}
+
+			} 
+			return izlaz;
 		}
 	}
-	
-	public static void ocisti(boolean sve)
+
+	public static String ocisti(boolean sve)
 	{
-		if (sve)
+		if (!InternaLogika.unos.isEmpty())
 		{
-			//Ovo jos nemamo ideje
-		} else
-		{
-			InternaLogika.unos.pop();
-			InternaLogika.unos.push((long)0);
+			if (sve)
+			{
+				InternaLogika.unos.clear(); //Ako cistimo sve brisemo skroz stack i nuliramo zadnju operaciju
+				InternaLogika.zadnjaOperacija = '0';
+			} else
+			{
+				InternaLogika.unos.pop(); //Ako brisemo samo zadnju gresku jedan pop() to resi i samo postavimo nulu na mesto te vrednosti
+				InternaLogika.unos.push((long)0);
+			}
 		}
+		return "0";
 	}
 }
--- src/digitron/MainWindow.java
+++ src/digitron/MainWindow.java
@@ -51,17 +51,16 @@ public class MainWindow {
 		frame.setBounds(100, 100, 434, 367);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		txtEkran = new JTextField();
 		txtEkran.setText("0");
-		txtEkran.addKeyListener(new KeyAdapter() 
+		txtEkran.addKeyListener(new KeyAdapter() //Slusamo tastaturu
 		{
 			@Override
-			public void keyTyped(KeyEvent arg0) 
+			public void keyTyped(KeyEvent unosSaTastature) //Kada korisnik otkuca nesto poziva se ova metoda
 			{
-					InternaLogika.procesirajUnos(arg0.getKeyChar());
-					osveziPrikaz();
-					arg0.consume();
+				txtEkran.setText(InternaLogika.procesirajUnos(unosSaTastature.getKeyChar())); //Pozivamo procesiranje unosa i povrat te metode postavljamo za displej 
+				unosSaTastature.consume(); //"Trosimo" ovaj dogadjaj da ne bi dozvolili standardnu obradu. Probajte da iskomentirate ovu liniju :) 
 			}
 		});
 		txtEkran.setBackground(Color.BLACK);
@@ -72,191 +71,199 @@ public class MainWindow {
 		frame.getContentPane().add(txtEkran);
 		txtEkran.setColumns(10);
 		JButton button = new JButton("0");
-		button.addActionListener(new ActionListener() 
+		button.addActionListener(new ActionListener() //Za svako dugme imamo listener koji ceka klik korisnika
 		{
 			public void actionPerformed(ActionEvent e) 
 			{
-				InternaLogika.procesirajUnos('0');
-				osveziPrikaz();
+				txtEkran.setText(InternaLogika.procesirajUnos('0')); //I svako dugme jednostavno salje svoj simbol na procesiranje i postvalja sta dobije nazad na ekran
+
 			}
 		});
 		button.setBounds(10, 289, 89, 23);
 		frame.getContentPane().add(button);
-		
+
 		JButton button_1 = new JButton("1");
 		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				InternaLogika.procesirajUnos('1');
-				osveziPrikaz();
+				txtEkran.setText(InternaLogika.procesirajUnos('1'));
+
 			}
 		});
 		button_1.setBounds(10, 255, 89, 23);
 		frame.getContentPane().add(button_1);
-		
+
 		JButton button_2 = new JButton("2");
 		button_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				InternaLogika.procesirajUnos('2');
-				osveziPrikaz();
+				txtEkran.setText(InternaLogika.procesirajUnos('2'));
+
 			}
 		});
 		button_2.setBounds(109, 255, 89, 23);
 		frame.getContentPane().add(button_2);
-		
+
 		JButton button_3 = new JButton("3");
 		button_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				InternaLogika.procesirajUnos('3');
-				osveziPrikaz();
+				txtEkran.setText(InternaLogika.procesirajUnos('3'));
+
 			}
 		});
 		button_3.setBounds(208, 255, 89, 23);
 		frame.getContentPane().add(button_3);
-		
+
 		JButton button_4 = new JButton("4");
 		button_4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				InternaLogika.procesirajUnos('4');
-				osveziPrikaz();
+				txtEkran.setText(InternaLogika.procesirajUnos('4'));
+
 			}
 		});
 		button_4.setBounds(10, 221, 89, 23);
 		frame.getContentPane().add(button_4);
-		
+
 		JButton button_5 = new JButton("5");
 		button_5.addActionListener(new ActionListener() 
 		{
 			public void actionPerformed(ActionEvent e) 
 			{	
-				InternaLogika.procesirajUnos('5');
-				osveziPrikaz();
+				txtEkran.setText(InternaLogika.procesirajUnos('5'));
+
 			}
 		});
 		button_5.setBounds(109, 221, 89, 23);
 		frame.getContentPane().add(button_5);
-		
+
 		JButton button_6 = new JButton("6");
 		button_6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				InternaLogika.procesirajUnos('6');
-				osveziPrikaz();
+				txtEkran.setText(InternaLogika.procesirajUnos('6'));
+
 			}
 		});
 		button_6.setBounds(208, 221, 89, 23);
 		frame.getContentPane().add(button_6);
-		
+
 		JButton button_7 = new JButton("7");
 		button_7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				InternaLogika.procesirajUnos('7');
-				osveziPrikaz();
+				txtEkran.setText(InternaLogika.procesirajUnos('7'));
+
 			}
 		});
 		button_7.setBounds(10, 187, 89, 23);
 		frame.getContentPane().add(button_7);
-		
+
 		JButton button_8 = new JButton("8");
 		button_8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				InternaLogika.procesirajUnos('8');
-				osveziPrikaz();
+				txtEkran.setText(InternaLogika.procesirajUnos('8'));
+
 			}
 		});
 		button_8.setBounds(109, 187, 89, 23);
 		frame.getContentPane().add(button_8);
-		
+
 		JButton button_9 = new JButton("9");
 		button_9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-					InternaLogika.procesirajUnos('9');
-					osveziPrikaz();
+
+				txtEkran.setText(InternaLogika.procesirajUnos('9'));
+
 			}
 		});
 		button_9.setBounds(208, 187, 89, 23);
 		frame.getContentPane().add(button_9);
-		
+
 		JButton btnC = new JButton("C");
+		btnC.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				txtEkran.setText(InternaLogika.ocisti(true)); //Jedino kod C i CE pozivamo drugu metodu koju za argument ima boolean koji odredjuje brisemo li sve ili samo poslednje
+			}
+		});
 		btnC.setBounds(10, 72, 89, 23);
 		frame.getContentPane().add(btnC);
-		
+
 		JButton btnCe = new JButton("CE");
 		btnCe.addActionListener(new ActionListener() 
 		{
 			public void actionPerformed(ActionEvent arg0) 
 			{
-				InternaLogika.ocisti(false);
-				osveziPrikaz();
+				txtEkran.setText(InternaLogika.ocisti(false));
 			}
 		});
 		btnCe.setBounds(109, 72, 89, 23);
 		frame.getContentPane().add(btnCe);
-		
+
 		JButton btnMc = new JButton("MC");
 		btnMc.setBounds(208, 72, 89, 23);
 		frame.getContentPane().add(btnMc);
-		
+
 		JButton btnM = new JButton("M+");
 		btnM.setBounds(10, 106, 89, 23);
 		frame.getContentPane().add(btnM);
-		
-		JButton btnM_1 = new JButton("M-");
-		btnM_1.setBounds(109, 106, 89, 23);
-		frame.getContentPane().add(btnM_1);
-		
+
+		JButton btnMm = new JButton("M-");
+		btnMm.setBounds(109, 106, 89, 23);
+		frame.getContentPane().add(btnMm);
+
 		JButton btnMr = new JButton("MR");
 		btnMr.setBounds(208, 106, 89, 23);
 		frame.getContentPane().add(btnMr);
-		
-		JButton button_10 = new JButton(".");
-		button_10.setBounds(109, 289, 89, 23);
-		frame.getContentPane().add(button_10);
-		
-		JButton button_11 = new JButton("%");
-		button_11.setBounds(208, 289, 89, 23);
-		frame.getContentPane().add(button_11);
-		
-		JButton btnNewButton = new JButton("=");
-		btnNewButton.setBounds(307, 289, 89, 23);
-		frame.getContentPane().add(btnNewButton);
-		
-		JButton button_12 = new JButton("+");
-		button_12.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) 
+
+		JButton button_tacka = new JButton(".");
+		button_tacka.setBounds(109, 289, 89, 23);
+		frame.getContentPane().add(button_tacka);
+
+		JButton button_proc = new JButton("%");
+		button_proc.setBounds(208, 289, 89, 23);
+		frame.getContentPane().add(button_proc);
+
+		JButton btn_jed = new JButton("=");
+		btn_jed.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0) 
 			{
-				InternaLogika.procesirajUnos('+');
-				osveziPrikaz();
+				txtEkran.setText(InternaLogika.procesirajUnos('='));
 			}
 		});
-		button_12.setBounds(307, 255, 89, 23);
-		frame.getContentPane().add(button_12);
+		btn_jed.setBounds(307, 289, 89, 23);
+		frame.getContentPane().add(btn_jed);
+
+		JButton button_sab = new JButton("+");
+		button_sab.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				txtEkran.setText(InternaLogika.procesirajUnos('+'));
+			}
+		});
+		button_sab.setBounds(307, 255, 89, 23);
+		frame.getContentPane().add(button_sab);
+
+		JButton button_min = new JButton("-");
+		button_min.setBounds(307, 221, 89, 23);
+		frame.getContentPane().add(button_min);
+
+		JButton button_put = new JButton("*");
+		button_put.setBounds(307, 187, 89, 23);
+		frame.getContentPane().add(button_put);
+
+		JButton button_pod = new JButton("/");
+		button_pod.setBounds(307, 153, 89, 23);
+		frame.getContentPane().add(button_pod);
+
+		JButton button_oZagrada = new JButton("(");
+		button_oZagrada.setBounds(208, 153, 42, 23);
+		frame.getContentPane().add(button_oZagrada);
+
+		JButton button_zZagrada = new JButton(")");
+		button_zZagrada.setBounds(260, 153, 42, 23);
+		frame.getContentPane().add(button_zZagrada);
 		
-		JButton button_13 = new JButton("-");
-		button_13.setBounds(307, 221, 89, 23);
-		frame.getContentPane().add(button_13);
-		
-		JButton button_14 = new JButton("*");
-		button_14.setBounds(307, 187, 89, 23);
-		frame.getContentPane().add(button_14);
-		
-		JButton button_15 = new JButton("/");
-		button_15.setBounds(307, 153, 89, 23);
-		frame.getContentPane().add(button_15);
-		
-		JButton button_16 = new JButton("(");
-		button_16.setBounds(208, 153, 42, 23);
-		frame.getContentPane().add(button_16);
-		
-		JButton button_17 = new JButton(")");
-		button_17.setBounds(260, 153, 42, 23);
-		frame.getContentPane().add(button_17);
-	}
-	
-	public void osveziPrikaz()
-	{
-		if (InternaLogika.unos.peek() != 0)
-		{
-			txtEkran.setText(String.valueOf(InternaLogika.unos.peek()));
-		}
+		JButton btnNegPoz = new JButton("+/-");
+		btnNegPoz.setBounds(109, 153, 89, 23);
+		frame.getContentPane().add(btnNegPoz);
 	}
 }
