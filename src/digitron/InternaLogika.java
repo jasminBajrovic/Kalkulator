2c3e2c94054f4f9f21f418138623ff7c5f811044
Dragan <d.pepic@hotmail.com>
2019-01-08 12:56:27 +0100

    Komentari :)

--- src/digitron/InternaLogika.java
+++ src/digitron/InternaLogika.java
@@ -28,8 +28,8 @@ import java.util.Stack;
 
 public class InternaLogika 
 {
-	static Stack<Double> unos = new Stack<Double>();
-	static Stack<String> operacije = new Stack<String>();
+	static Stack<Double> unos = new Stack<Double>(); //Stack u kome pamtimo brojeve
+	static Stack<String> operacije = new Stack<String>(); //Stack za operacije
 	static int broj;
 	static double zadnjiClan; 
 
@@ -37,45 +37,61 @@ public class InternaLogika
 	{
 		try
 		{
-			broj = Integer.parseInt(String.valueOf(unos)); 
+			broj = Integer.parseInt(String.valueOf(unos)); //Pokusavamo da procitamo broj
 
-			if (InternaLogika.unos.isEmpty()) 
+			if (InternaLogika.unos.isEmpty()) //Ako uspemom i ako nemamo ni jedan, ide na stack
 			{
 				InternaLogika.unos.push((double)broj); 
 			} else
 			{
+				//Ako vec imamo nesto na stacku, onda to pomeramo za desetku 
+				//i dodajemo novi broj (npr, imali 2, kucamo 1, dobijamo 2*10+1 = 21)
 				InternaLogika.unos.push(InternaLogika.unos.pop()*10 + broj);
 			}
-			return ukloniNule(String.valueOf(InternaLogika.unos.peek())); 
+			return ukloniNule(String.valueOf(InternaLogika.unos.peek()));
+			//Vracamo unos provucen kroz metodu koja nam brise pratece nule, ako ih ima
 		} catch (NumberFormatException e)
 		{
-			String izlaz = "0"; 
-			if (!InternaLogika.unos.isEmpty())
+			String izlaz = "0"; //Ako nije broj, nadjemo se ovde
+			                   //odmah pripremimo 0 za izlaz, u slucaju da ne uradimo
+			                   //ama nista
+			if (InternaLogika.unos.size() >= 2) //Ako imamo nekih brojeva na stacku
 			{
-				if (unos == '=')
+				if (unos == '=') //pritisnuli smo jednako
 				{
-					if (!InternaLogika.operacije.isEmpty()) 
+					if (!InternaLogika.operacije.isEmpty()) //Imamo neku operaciju vec 
 					{
-						if (InternaLogika.zadnjiClan == 0)
-						{
+						if (InternaLogika.zadnjiClan == 0) //ako nismo vec
+						{                         //pamtimo zadnji clan da bi 
+							                      //mogli da ponavljamo =
 							InternaLogika.zadnjiClan = InternaLogika.unos.peek();
 						} else
 						{
+							//Ako ga imamo upamcenog, onda ga vracamo na stack
 							InternaLogika.unos.push(zadnjiClan);
 						}
+						//Pogledamo koja je operacija u pitanju ali je ne izvrsavamo
 						InternaLogika.izvrsiOperaciju(InternaLogika.operacije.peek());
+						//Spremamo izlaz
 						izlaz = String.valueOf(InternaLogika.unos.peek());
 					}
-				} else
+				} else //Nismo pritisnuli jednako
 				{
-					InternaLogika.zadnjiClan = 0;
+					InternaLogika.zadnjiClan = 0; //anuliramo clan koji nam
+					                        //sluzi za ponavljanje =
 					InternaLogika.operacije.push(String.valueOf(unos));
+					//postavljamo operaciju koja je pritisnuta na stack
+					
 					if (InternaLogika.unos.size() < 2) 
 					{
+						//Ako imamo manje od 2 clana na stacku nemamo sta da radimo,
+						//ali saljemo zadnji podatak sa stacka na prikaz
 						izlaz = String.valueOf(InternaLogika.unos.peek()); 
 					} else
 					{
 						int indeksOperacije;
+						//Sada nas intresuje da li treba da izvrsimo zadnju ili predzadnju
+						//operaciju
 						if (InternaLogika.operacije.size() > 1)
 						{
 							indeksOperacije = operacije.size() - 2;
@@ -83,20 +99,24 @@ public class InternaLogika
 						{
 							indeksOperacije = operacije.size() - 1;
 						}
+						
+						//Izvrsavamo je i postavljamo izlaz
 						InternaLogika.izvrsiOperaciju(InternaLogika.operacije.get(indeksOperacije));
 						InternaLogika.operacije.remove(indeksOperacije);
 						izlaz = String.valueOf(InternaLogika.unos.peek());
 					}
-					InternaLogika.unos.push((double)0);
+					
+					InternaLogika.unos.push((double)0); //Unosimo nulu koja ce da sluzi 
+					                                 //za iduci broj, posle operacije
 				}
 			}
-			return ukloniNule(izlaz);
+			return ukloniNule(izlaz); 
 		}
 	}
 
 	public static void izvrsiOperaciju(String op)
 	{
-		double drugi;
+		double drugi; //treba nam za / i -
 		switch (op)
 		{
 		case "+": 
@@ -117,21 +137,25 @@ public class InternaLogika
 	}
 	public static String ukloniNule(String broj)
 	{
-		while (broj.contains("."))
+		while (broj.contains(".")) //Dok broj sadrzi tacku
 		{
-			if (broj.endsWith("0"))
+			if (broj.endsWith("0")) 
 			{
-
+				//brisi pratece nule
 				broj = broj.substring(0, broj.length()-2); 
 			}else if (broj.endsWith("."))
 			{
+				//Ako smo stigli do tacke (npr. bilo 8.00, sada je 8.)
+				//ukloni i nju
 				broj = broj.substring(0, broj.length()-2);
 			} else
 			{
+				//Nesto drugo nam je na kraju, npr 8.2, to je ok, vracamo broj
 				return broj;
 			}
 		}
-		return broj;
+		return broj; //Ako ne sadrzi tacku nemamo nule koje bi brisali pa samo
+		             //vracamo br :) 
 	}
 
 
